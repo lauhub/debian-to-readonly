bfb0c2dc0e7fd33d28b8bb9caa93e3b96defaaff
Laurent HUBERT <lau.hub@gmail.com>
2015-10-30 18:37:34 +0100

    Added missing fstab file, automatic configuration script

--- systemtoreadonly.sh
+++ systemtoreadonly.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+mkdir -p /local/home /local/srv 
+
+#Creates the places where we will bind original files to:
+mkdir -p /ro/var /ro/srv /ro/home
+
+
+
 apt-get remove --purge x11-common alsa-base alsa-utils apache2 lxsession lxpanel gvfs openbox aspell aspell-en vlc gnome-mplayer desktop-base cupsys-client cupsys-bsd foomatic-filters hplip
 apt-get autoremove --purge
 
@@ -7,3 +14,7 @@ rm /etc/adjtime; ln -s /var/local/adjtime /etc/adjtime
 mv /etc/init.d/hwclock.sh /etc/init.d/hwclock.sh.bak && cat /etc/init.d/hwclock.sh.bak | sed 's#\(HWCLOCKPARS=\)\(.*$\)#\1"\2 --noadjfile"#g' > /etc/init.d/hwclock.sh && chmod +x /etc/init.d/hwclock.sh
 cp /etc/environment /etc/environment.bak && echo "BLKID_FILE=/var/local/blkid.tab" >> /etc/environment
 mv /etc/resolv.conf /var/tmp/resolv.conf && ln -s /var/tmp/resolv.conf /etc/resolv.conf
+
+#Replaces the dhclient-script which writes into /etc/resolv.conf
+cp config-files/dhclient-script /etc/dhclient-script
+
