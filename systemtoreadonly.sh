f7f25e930e61b116a31c27e38d40360475c5cd71
Laurent HUBERT <lau.hub@gmail.com>
2015-10-31 11:01:33 +0100

    Modified bad path for resolv.conf

--- systemtoreadonly.sh
+++ systemtoreadonly.sh
@@ -8,6 +8,6 @@ cp /etc/adjtime /var/local/adjtime
 rm /etc/adjtime; ln -s /var/local/adjtime /etc/adjtime
 mv /etc/init.d/hwclock.sh /etc/init.d/hwclock.sh.bak && cat /etc/init.d/hwclock.sh.bak | sed 's#\(HWCLOCKPARS=\)\(.*$\)#\1"\2 --noadjfile"#g' > /etc/init.d/hwclock.sh && chmod +x /etc/init.d/hwclock.sh
 cp /etc/environment /etc/environment.bak && echo "BLKID_FILE=/var/local/blkid.tab" >> /etc/environment
-mv /etc/resolv.conf /var/tmp/resolv.conf && ln -s /var/tmp/resolv.conf /etc/resolv.conf
+mv /etc/resolv.conf /tmp/resolv.conf && ln -s /tmp/resolv.conf /etc/resolv.conf
 
 
